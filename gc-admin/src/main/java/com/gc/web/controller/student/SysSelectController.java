1b0e09ae41a45c8fdb797825a9dce0c683a1f83e
AwayFuture <15627916068@qq.com>
2020-02-20 21:13:49 +0800

    新增功能接口: 获取课程列表list

--- gc-admin/src/main/java/com/gc/web/controller/student/SysSelectController.java
+++ gc-admin/src/main/java/com/gc/web/controller/student/SysSelectController.java
@@ -1,10 +1,19 @@
 package com.gc.web.controller.student;
 
 import com.gc.common.core.controller.BaseController;
+import com.gc.common.core.page.TableDataInfo;
+import com.gc.system.domain.GcSchedule;
+import com.gc.system.service.GcSelectService;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 学生中心 - 选课入口
@@ -17,10 +26,29 @@ public class SysSelectController extends BaseController {
 
     private String prefix = "student/select";
 
+    @Autowired
+    private GcSelectService gcSelectService;
+
+    /**
+     * 获取选课入口页面
+     **/
     @RequiresPermissions("student:select:view")
     @GetMapping()
     public String select() {
         return prefix + "/select";
     }
+
+    /**
+     * 获取课程列表
+     **/
+    // @RequiresPermissions("")
+    @PostMapping("/list")
+    @ResponseBody
+    public TableDataInfo list(GcSchedule gcSchedule) {
+        startPage();
+        List<GcSchedule> list = gcSelectService.selectScheduleList(gcSchedule);
+        return getDataTable(list);
+    }
+
 
 }
